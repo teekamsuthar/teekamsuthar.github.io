303ae8d0d3c6e534258dca824b521975de077cde
teekamsuthar <teekam.suthar1@gmail.com>
2021-04-05 22:25:32 +0530

    fix mobile html renderer issues

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -53,9 +53,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "a2f8062e94d20f76778fef6b48295b2f",
-"/": "a2f8062e94d20f76778fef6b48295b2f",
-"main.dart.js": "58bc4413d0407dcd0a623dafa39fc2f4",
+"index.html": "c8d29e769c38a7a094f4cb51b696aad3",
+"/": "c8d29e769c38a7a094f4cb51b696aad3",
+"main.dart.js": "7289218a12bde0860bf07f4213dda22b",
 "manifest.json": "19048c388858d02a933e79a393fe1d38",
 "version.json": "49ccc44401fdd5554d017ba8c8cc1cbf"
 };
